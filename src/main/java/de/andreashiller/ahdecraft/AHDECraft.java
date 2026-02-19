296026a4ee4dc7fec6c9a8c3533861598fab2683
Andreas Hiller <kontakt@andreashiller.de>
2026-02-19 23:50:51 +0100

    Initial commit

---
+++ src/main/java/de/andreashiller/ahdecraft/AHDECraft.java
@@ -0,0 +1,16 @@
+package de.andreashiller.ahdecraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AHDECraft implements ModInitializer {
+	public static final String MOD_ID = "ahdecraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/de/andreashiller/ahdecraft/AHDECraftClient.java
@@ -0,0 +1,10 @@
+package de.andreashiller.ahdecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AHDECraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
